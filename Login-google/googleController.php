c34ac9814791878ce866b4c8fcaa40aac4f5b47c
Juan Martin Roman <105949296+gitgodjs@users.noreply.github.com>
2024-09-02 13:44:58 -0300

    Code!
    This code omits specifics like the environment, route/service.php, and API endpoint serving. For more details, check out this document: https://docs.google.com/document/d/10Ab-p4GryZrjsUXuMadV6VLBSzhFrthKGWkooEn9LL8/edit?usp=sharing

---
+++ Login-google/googleController.php
@@ -0,0 +1,44 @@
+use Laravel\Socialite\Facades\Socialite;
+
+
+// En este caso tenemos un controlador que se llama AuthController y manejamos los tokens atraves de auth()
+public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->stateless()->redirect();
+    }
+
+    public function handleGoogleCallback(Request $request)
+    {
+    // Obtener el usuario de Google
+    $googleUser = Socialite::driver('google')->stateless()->user();
+    $googleId = $googleUser->id;
+    $googleEmail = $googleUser->email;
+        try {
+            
+
+            // Verificar si el usuario ya existe en la base de datos
+            $existingUser = User::where('google_id', $googleId)->first();
+            if ($existingUser) {
+                // Si el usuario existe, iniciar sesión y generar un token
+                $token = auth()->login($existingUser);
+            } else {
+                // Si el usuario no existe, crear uno nuevo y generar un token
+                $user = User::create([
+                    'correo' => $googleEmail,
+                    'google_id' => $googleId,
+                    'password' => $googleId, // Contraseña ficticia para nuevos usuarios
+                ]);
+                $token = auth()->login($user);
+            }
+
+            // Enviar el token al frontend (puedes redireccionar o responder con JSON)
+            return redirect('http://localhost:3000/home?token=' . $token); // Redireccionar al frontend con el token
+
+        } catch (Throwable $e) {
+            // Verificar si el error se debe a un correo duplicado (puedes agregar lógica más robusta aquí)
+            if (str_contains($e->getMessage(), 'Duplicate entry')) {
+                // Si el error es por correo duplicado, retornar una uri especifica
+                return redirect('http://localhost:3000/signup?error=email_in_use');
+            }
+        }
+    }
